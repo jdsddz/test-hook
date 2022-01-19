f722e1350310ff741a1e1ab14ea267a1dcf3b949
Yuebo Li <Alex@Yuebos-MacBook-Pro.local>
2022-01-19 08:59:15 -0500

    test

--- pre_commit_hooks/dummy_check.py
+++ pre_commit_hooks/dummy_check.py
@@ -25,10 +25,14 @@ def main(argv=None):
     # files_str = run_process('pwd')
     # print(f'files_str: {files_str}')
 
-    p = os.popen('pip list --format=json')
+    # p = run_process('pip list --format=json')
 
     # dependencies = []
-    print(f'p: {p}')
+    # print(f'p: {p}')
+    p = Pipfile.load('Pipfile')
+    print(p.contents)
+    print(p.lock())
+
     # if 'Pipfile' in files_str:
     #     dependencies = run_process('pip freeze').split('\n')
     # elif 'pom.xml' in files_str:
