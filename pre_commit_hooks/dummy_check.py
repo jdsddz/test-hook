5eedd89cb362270b584b0e44706b48f6f22f45b3
Yuebo Li <Alex@Yuebos-MacBook-Pro.local>
2022-01-18 22:27:27 -0500

    test

--- pre_commit_hooks/dummy_check.py
+++ pre_commit_hooks/dummy_check.py
@@ -1,9 +1,14 @@
 import sys
+import argparse
 
 
 def main(argv=None):
+  parser = argparse.ArgumentParser()
+  parsed_args, args_rest = parser.parse_known_args(argv)
+  print(f'parsed_args: {parsed_args}')
+  print(f'args_rest: {args_rest}')
   print(f'aaaaadbdd')
-  return 1
+  return 1234
 
 
 if __name__ == "__main__":
