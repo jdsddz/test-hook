457797947b5e3586806fd927ad70083de3b51add
Yuebo Li <Alex@Yuebos-MacBook-Pro.local>
2022-01-19 08:52:19 -0500

    test

--- pre_commit_hooks/dummy_check.py
+++ pre_commit_hooks/dummy_check.py
@@ -22,7 +22,7 @@ def main(argv=None):
     #   print(f'file_path: {file_path}')
     # stream = os.popen('ls')
     # output = stream.read()
-    files_str = run_process('ls')
+    files_str = run_process('pwd')
     print(f'files_str: {files_str}')
 
     p = run_process('python -m pip list --format=json')
