91c7f1abb19765d454d4ceb4e6149f1e93c9017a
Yuebo Li <Alex@Yuebos-MacBook-Pro.local>
2022-01-19 08:25:29 -0500

    test

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='test-hook',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=['pipfile'],
     entry_points={
         'console_scripts': [
             'dummy_check = pre_commit_hooks.dummy_check:main',
