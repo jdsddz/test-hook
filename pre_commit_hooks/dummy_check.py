07498b24b08a6af45f1f2a3a1d0a7d4badfbd929
Yuebo Li <Alex@Yuebos-MacBook-Pro.local>
2022-01-18 23:45:37 -0500

    tes

--- pre_commit_hooks/dummy_check.py
+++ pre_commit_hooks/dummy_check.py
@@ -1,41 +1,32 @@
 import sys
 import argparse
 import os
+import subprocess
 
-def build_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--full-report",
-        default="--full-report",
-        const="--full-report",
-        action="store_const",
-    )
-    parser.add_argument(
-        "--short-report",
-        dest="full_report",
-        const="--short-report",
-        action="store_const",
-    )
-    parser.add_argument("--ignore", "-i", action="append")
-    parser.add_argument("files", nargs="+")
-    return parser
+
+def run_process(cmd_arg):
+    with subprocess.Popen(cmd_arg, stdout=subprocess.PIPE, shell=True) as proc:
+        return proc.stdout.read()
 
 
 def main(argv=None):
-  parser = build_parser()
-  # parser = argparse.ArgumentParser()
-  parsed_args, args_rest = parser.parse_known_args(argv)
-  print(f'parsed_args: {parsed_args}')
-  print(f'args_rest: {args_rest}')
-  for file_path in parsed_args.files:
-    print(f'file_path: {file_path}')
-  stream = os.popen('ls')
-  output = stream.read()
-  print(f'output: {output}')
-  print(f'aaaaadbdd')
-  print(type(output))
-  return 1234
+    # parser = build_parser()
+    # parser = argparse.ArgumentParser()
+    # parsed_args, args_rest = parser.parse_known_args(argv)
+    # print(f'parsed_args: {parsed_args}')
+    # print(f'args_rest: {args_rest}')
+    # for file_path in parsed_args.files:
+    #   print(f'file_path: {file_path}')
+    # stream = os.popen('ls')
+    # output = stream.read()
+    if run_process('ls -d pipfile') == 'pipfile':
+        dependencies = run_process('pip freeze').split('\n')
+        print (f'list: {dependencies}')
+    # print(f'output: {output}')
+    print(f'aaaaadbdd')
+    # print(type(output))
+    return 1234
 
 
 if __name__ == "__main__":
-  sys.exit(main())
+    sys.exit(main())
