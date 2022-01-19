a76f54bd67c543abfdc117b5d708328df0e7e721
Yuebo Li <Alex@Yuebos-MacBook-Pro.local>
2022-01-18 20:35:55 -0500

    test

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
 from setuptools import find_packages
 from setuptools import setup
 
-setup(
-    name='test-hook',
-    packages=find_packages('.'),
-    entry_points={
-        'console_scripts': [
-            'dummy_check = dummy_check:main',
-        ],
-    },
-)
+setup()
