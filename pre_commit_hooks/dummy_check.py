61b2210265dd66f451fdd3dab092085fe223c638
Yuebo Li <Alex@Yuebos-MacBook-Pro.local>
2022-01-19 08:56:32 -0500

    tes

--- pre_commit_hooks/dummy_check.py
+++ pre_commit_hooks/dummy_check.py
@@ -25,7 +25,7 @@ def main(argv=None):
     # files_str = run_process('pwd')
     # print(f'files_str: {files_str}')
 
-    p = os.popen('python -m pip list --format=json')
+    p = os.popen('pip list --format=json')
 
     # dependencies = []
     print(f'p: {p}')
